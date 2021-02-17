b44c684b2a36140d564474324f46198b182f688e
Alinur Zhengis <62282182+zhengis-alinur@users.noreply.github.com>
2021-02-17 23:32:34 +0600

    add black keys

--- Virtual Piano/task/src/script.js
+++ Virtual Piano/task/src/script.js
@@ -1,56 +1,125 @@
 let sound;
+let A = document.getElementById("A");
+let S = document.getElementById("S");
+let D = document.getElementById("D");
+let F = document.getElementById("F");
+let G = document.getElementById("G");
+let H = document.getElementById("H");
+let J = document.getElementById("J");
+let W = document.getElementById("W");
+let E = document.getElementById("E");
+let T = document.getElementById("T");
+let Y = document.getElementById("Y");
+let U = document.getElementById("U");
+
 document.addEventListener("keydown", function(event) {
   switch (event.code){
     case "KeyA":
+        A.style.backgroundColor = "gray";
         sound = new Audio("sounds/A.mp3");
         sound.play();
         break;
     case "KeyS":
+        S.style.backgroundColor = "gray";
         sound = new Audio("sounds/S.mp3");
         sound.play();
         break;
     case "KeyD":
+        D.style.backgroundColor = "gray";
         sound = new Audio("sounds/D.mp3");
         sound.play();
         break;
     case "KeyF":
+        F.style.backgroundColor = "gray";
         sound = new Audio("sounds/F.mp3");
         sound.play();
         break;
     case "KeyG":
+        G.style.backgroundColor = "gray";
         sound = new Audio("sounds/G.mp3");
         sound.play();
         break;
     case "KeyH":
+        H.style.backgroundColor = "gray";
         sound = new Audio("sounds/H.mp3");
         sound.play();
         break;
     case "KeyJ":
+        J.style.backgroundColor = "gray";
         sound = new Audio("sounds/J.mp3");
         sound.play();
         break;
     case "KeyW":
-        sound = new Audio("sounds/J.mp3");
+        W.style.backgroundColor = "#504E4E";
+        sound = new Audio("sounds/W.mp3");
         sound.play();
         break;
     case "KeyE":
-        sound = new Audio("sounds/J.mp3");
+        E.style.backgroundColor = "#504E4E";
+        sound = new Audio("sounds/E.mp3");
         sound.play();
         break;
     case "KeyT":
-        sound = new Audio("sounds/J.mp3");
+        T.style.backgroundColor = "#504E4E";
+        sound = new Audio("sounds/T.mp3");
         sound.play();
         break;
     case "KeyY":
-        sound = new Audio("sounds/J.mp3");
+        Y.style.backgroundColor = "#504E4E";
+        sound = new Audio("sounds/Y.mp3");
         sound.play();
         break;
     case "KeyU":
-        sound = new Audio("sounds/J.mp3");
+        U.style.backgroundColor = "#504E4E";
+        sound = new Audio("sounds/U.mp3");
         sound.play();
         break;
     default:
         console.log("Warning");
         break;
   }
+});
+
+document.addEventListener("keyup", function(event) {
+    switch (event.code){
+        case "KeyA":
+            A.style.backgroundColor = "white";
+            break;
+        case "KeyS":
+            S.style.backgroundColor = "white";
+            break;
+        case "KeyD":
+            D.style.backgroundColor = "white";
+            break;
+        case "KeyF":
+            F.style.backgroundColor = "white";
+            break;
+        case "KeyG":
+            G.style.backgroundColor = "white";
+            break;
+        case "KeyH":
+            H.style.backgroundColor = "white";
+            break;
+        case "KeyJ":
+            J.style.backgroundColor = "white";
+            break;
+        case "KeyW":
+            W.style.backgroundColor = "black";
+            break;
+        case "KeyE":
+            E.style.backgroundColor = "black";
+            break;
+        case "KeyT":
+            T.style.backgroundColor = "black";
+            break;
+        case "KeyY":
+            Y.style.backgroundColor = "black";
+            break;
+        case "KeyU":
+            U.style.backgroundColor = "black";
+            break;
+        default:
+            console.log("Warning");
+            break;
+    }
 });
